df69dafea52069b921aefe12841244bb207bcc27
junhyung8795 <junhyung8795@naver.com>
2026-04-07 13:52:08 +0900

    feat: 초기 kpop 굿즈 예약 프로젝트 세팅

---
+++ src/test/java/com/park/kpopGoodsReservation/KpopGoodsReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.park.kpopGoodsReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpopGoodsReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
